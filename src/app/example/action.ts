0c6c6f10a184eeedcf0c2436ffb96d09ecdcfb0e
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-03-29 21:15:14 +0100

    Show specific exception message

--- src/app/example/action.ts
+++ src/app/example/action.ts
@@ -2,10 +2,12 @@
 
 import { schema } from "@/app/example/schema";
 import { makeServerAction } from "@/lib/server/makeServerAction";
+// import { Exception } from "@/lib/exception/Exception";
 
 export const action = makeServerAction(schema, async ({ email }) => {
   console.log(`Processing form on the server with email "${email}"…`);
 
   // throw new Error("Some unexpected error.");
+  // throw new Exception("Specific exception message.");
   return { message: `Processed ${email} successfully.` };
 });
--- src/lib/server/makeServerAction.ts
+++ src/lib/server/makeServerAction.ts
@@ -1,5 +1,9 @@
 import { ZodSchema } from "zod";
-import { createSafeActionClient } from "next-safe-action";
+import {
+  createSafeActionClient,
+  DEFAULT_SERVER_ERROR_MESSAGE,
+} from "next-safe-action";
+import { Exception } from "@/lib/exception/Exception";
 
 /**
  * Provides a server action ready to use in `useFormServerAction` (`src/lib/browser/useFormServerAction.tsx`)
@@ -13,7 +17,23 @@ export function makeServerAction<Input, ReturnValue>(
   inputSchema: ZodSchema<Input>,
   handler: (input: Input) => Promise<ReturnValue>
 ) {
-  return createSafeActionClient()
+  return createSafeActionClient({
+    handleServerError(error) {
+      const isException = error instanceof Exception;
+
+      console.error(
+        `${isException ? "⚠️ Action exception" : "❌ Action error"}: ${
+          error.message
+        }`
+      );
+
+      if (isException) {
+        return error.message;
+      }
+
+      return DEFAULT_SERVER_ERROR_MESSAGE;
+    },
+  })
     .schema(inputSchema)
     .action(async ({ parsedInput }) => handler(parsedInput));
 }
